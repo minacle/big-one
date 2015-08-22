01d8321d8777a36519c364f454c707d4c3cc819b
Sinoru <sinoru@me.com>
2015-08-22 19:06:41 +0900

    Add Objective-C implementation

---
+++ Objective-C/sinoru.m
@@ -0,0 +1,27 @@
+//
+//  sinoru.m
+//  
+//
+//  Created by Sinoru on 2015. 8. 22..
+//  Copyright © 2015년 Sinoru. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSOutputStream *outputStream = [[NSOutputStream alloc] initToFileAtPath:[[[NSFileManager defaultManager] currentDirectoryPath] stringByAppendingPathComponent:@"big"] append:YES];
+        if (outputStream != nil) {
+            [outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+            [outputStream open];
+            
+            NSInteger status = 0;
+            NSData *data = [@"\0" dataUsingEncoding:NSUTF8StringEncoding];
+            while (status >= 0) {
+                status = [outputStream write:data.bytes maxLength:1];
+            }
+            NSLog(@"%@", outputStream.streamError);
+        }
+    }
+    return 0;
+}
