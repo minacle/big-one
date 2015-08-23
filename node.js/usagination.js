c9a9bd2dded151265c3d4fe0dbd57a6167067fd8
Mayu Laierlence <minacle@live.com>
2015-08-23 19:23:22 +0900

    0 to \0

--- node.js/usagination.js
+++ node.js/usagination.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 while (true) {
     try {
         var fd = fs.openSync('./big', 'a');
-        fs.writeSync(fd, 0);
+        fs.writeSync(fd, '\0');
         fs.closeSync(fd);
     } catch (e) {
         break;
